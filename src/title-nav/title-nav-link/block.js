b161a8fceb832e04834c5d96341c0533542a493a
jyoung295 <young295@purdue.edu>
2020-07-22 11:37:20 -0400

    add image-showcase and title nav blocks

---
+++ src/title-nav/title-nav-link/block.js
@@ -0,0 +1,137 @@
+/* eslint-disable react/jsx-key */
+
+/**
+ * BLOCK: Bulma Container
+ *
+ * Bulma container block: https://bulma.io/documentation/layout/container/.
+ */
+
+//  Import CSS.
+import './editor.scss';
+import './style.scss';
+
+const { __ } = wp.i18n; // Import __() from wp.i18n
+const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+
+const {
+  PanelBody,
+  PanelRow,
+  CheckboxControl,
+} = wp.components;
+
+const { RichText, InspectorControls } = wp.blockEditor;
+
+/**
+ * Register: aa Gutenberg Block.
+ *
+ * Registers a new block provided a unique name and an object defining its
+ * behavior. Once registered, the block is made editor as an option to any
+ * editor interface where blocks are implemented.
+ *
+ * @link https://wordpress.org/gutenberg/handbook/block-api/
+ * @param  {string}   name     Block name.
+ * @param  {Object}   settings Block settings.
+ * @return {?WPBlock}          The block, if it has been successfully
+ *                             registered; otherwise `undefined`.
+ */
+registerBlockType( 'purdue-blocks/title-nav-link', {
+  // Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
+  title: __( 'Titled Navigation Link' ), // Block title.
+  icon: (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      data-prefix="far"
+      data-icon="link"
+      className="svg-inline--fa fa-link fa-w-16"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+    >
+      <path
+        fill="#8E6F3E"
+        d="M326.612 185.391c59.747 59.809 58.927 155.698.36 214.59-.11.12-.24.25-.36.37l-67.2 67.2c-59.27 59.27-155.699 59.262-214.96 0-59.27-59.26-59.27-155.7 0-214.96l37.106-37.106c9.84-9.84 26.786-3.3 27.294 10.606.648 17.722 3.826 35.527 9.69 52.721 1.986 5.822.567 12.262-3.783 16.612l-13.087 13.087c-28.026 28.026-28.905 73.66-1.155 101.96 28.024 28.579 74.086 28.749 102.325.51l67.2-67.19c28.191-28.191 28.073-73.757 0-101.83-3.701-3.694-7.429-6.564-10.341-8.569a16.037 16.037 0 0 1-6.947-12.606c-.396-10.567 3.348-21.456 11.698-29.806l21.054-21.055c5.521-5.521 14.182-6.199 20.584-1.731a152.482 152.482 0 0 1 20.522 17.197zM467.547 44.449c-59.261-59.262-155.69-59.27-214.96 0l-67.2 67.2c-.12.12-.25.25-.36.37-58.566 58.892-59.387 154.781.36 214.59a152.454 152.454 0 0 0 20.521 17.196c6.402 4.468 15.064 3.789 20.584-1.731l21.054-21.055c8.35-8.35 12.094-19.239 11.698-29.806a16.037 16.037 0 0 0-6.947-12.606c-2.912-2.005-6.64-4.875-10.341-8.569-28.073-28.073-28.191-73.639 0-101.83l67.2-67.19c28.239-28.239 74.3-28.069 102.325.51 27.75 28.3 26.872 73.934-1.155 101.96l-13.087 13.087c-4.35 4.35-5.769 10.79-3.783 16.612 5.864 17.194 9.042 34.999 9.69 52.721.509 13.906 17.454 20.446 27.294 10.606l37.106-37.106c59.271-59.259 59.271-155.699.001-214.959z"
+      ></path>
+    </svg>
+  ), // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+  category: 'purdue-blocks', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+  keywords: [],
+  parent: [ 'purdue-blocks/title-nav' ],
+
+  /**
+   * The edit function describes the structure of your block in the context of the editor.
+   * This represents what the editor will render when the block is used.
+   *
+   * The "edit" property must be a valid function.
+   *
+   * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+   *
+   * @param {Object} props Props.
+   * @returns {Mixed} JSX Component.
+   */
+
+  attributes: {
+    linkText: { source: 'html', selector: 'li' },
+    isCurrent: { type: 'boolean', default: false },
+  },
+
+  supports: {
+    className: false,
+  },
+
+  // Block description in side panel
+  description: __(
+    'Add this block and select navigation items to build a menu that will show above the title of the page.'
+  ),
+
+  edit: ( props ) => {
+    return [
+      <InspectorControls>
+        <PanelBody>
+          <PanelRow>
+            <CheckboxControl
+              label="Is this the current page?"
+              checked={ props.attributes.isCurrent }
+              onChange={ ( checked ) =>
+                props.setAttributes( { isCurrent: checked } )
+              }
+            />
+          </PanelRow>
+        </PanelBody>
+      </InspectorControls>,
+
+      <div className={ 'editor-title-nav-link' }>
+        <RichText
+          tagname="li"
+          value={ props.attributes.linkText }
+          className={ `editor-title-nav-link__input ${ props.attributes.isCurrent ? 'isCurrent' : '' }` }
+          onChange={ ( text ) => {
+            props.setAttributes( { linkText: text } )
+          } }
+        >
+        </RichText>
+      </div>,
+    ];
+  },
+
+  /**
+   * The save function defines the way in which the different attributes should be combined
+   * into the final markup, which is then serialized by Gutenberg into post_content.
+   *
+   * The "save" property must be specified and must be a valid function.
+   *
+   * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+   *
+   * @param {Object} props Props.
+   * @returns {Mixed} JSX Frontend HTML.
+   */
+  save: ( props ) => {
+    return (
+      <RichText.Content
+        className={ `${ props.attributes.isCurrent ? 'isCurrent' : '' }` }
+        tagName="li"
+        value={ props.attributes.linkText }
+      />
+    )
+  },
+} );
