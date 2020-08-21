78a23a9e00346357fca7309b40352be88290ac61
Jesse Young <young295@purdue.edu>
2020-08-21 16:23:27 -0400

    add initial test

---
+++ src/title-hero/block.spec.js
@@ -0,0 +1,25 @@
+// title-hero test
+import {
+    createNewPost,
+    enablePageDialogAccept,
+    getEditedPostContent,
+    insertBlock,
+} from '@wordpress/e2e-test-utils';
+
+describe( 'Title Hero Block', () => {
+    beforeAll( async () => {
+        await enablePageDialogAccept();
+    } );
+    beforeEach( async () => {
+        await createNewPost();
+    } );
+
+    // Tests can be added here by using the it() function
+    it( 'Block should be available.', async () => {
+        await insertBlock( 'Title Hero' )
+
+        expect(await page.$('[data-type="purdue-blocks/title-hero"]')).not.toBeNull()
+
+        expect( await getEditedPostContent()).toMatchSnapshot()
+    } )
+} );
