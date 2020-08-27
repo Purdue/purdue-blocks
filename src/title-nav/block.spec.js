874bc947d8040d9d18478ad50cd8069531e05736
Jesse Young <young295@purdue.edu>
2020-08-27 15:15:09 -0400

    add tests for title nav and title nav links child

--- src/title-nav/block.spec.js
+++ src/title-nav/block.spec.js
@@ -10,7 +10,9 @@ import {
 import {
     clickElementByText,
     blockStartup,
-    updateRangeInput
+    updateRangeInput,
+    clickCheckbox,
+    openSidebarPanelWithTitle
 } from '../test-helpers'
 
 const block = {blockTitle: 'Titled Navigation', blockName: 'purdue-blocks/title-nav'}
@@ -63,5 +65,80 @@ describe( 'Titled Navigation Block', () => {
 
         expect( await getEditedPostContent() ).toMatchSnapshot()
 
+    })
+
+    it( 'Outline should be added correctly.', async () => {
+        await blockStartup(block)
+
+        //click to check the checkbox, outline should be on
+        await clickCheckbox('Give Menu Items an Outline?')
+        
+        let editedContent = await getEditedPostContent()
+        
+        expect( editedContent.includes('{"hasOutline":true}')).toBe(true)
+        expect( await getEditedPostContent() ).toMatchSnapshot()
+        
+
+        // click to uncheck the checkbox, outline should now turn off
+        await clickCheckbox('Give Menu Items an Outline?')
+        
+        editedContent = await getEditedPostContent()
+        
+        expect( editedContent.includes('{"hasOutline":true}')).toBe(false)
+        expect( await getEditedPostContent() ).toMatchSnapshot()
+    })
+
+    it( 'CTA button checkbox should toggle controls.', async () => {
+        await blockStartup(block)
+
+        // click the checkbox to show button controls
+        await clickCheckbox('Add a CTA Button?')
+        // incase the controls are not expanded by default
+        await openSidebarPanelWithTitle('Button Controls')
+
+        const buttonControls = await page.$('.components-panel__body.is-opened')
+
+        expect(buttonControls).not.toBeNull()
+
+    })
+
+    it( 'CTA button controls should update output correctly.', async () => {
+        await blockStartup(block)
+
+        // click the checkbox to show button controls
+        await clickCheckbox('Add a CTA Button?')
+        // incase the controls are not expanded by default
+        await openSidebarPanelWithTitle('Button Controls')
+
+        const typeString = 'Button Text'
+        const typeUrl = 'https://www.purdue.edu'
+
+        await clickElementByText('label', 'Button Text')
+        await page.keyboard.type(typeString, {delay: 10})
+
+        await clickElementByText('label', 'Button Link')
+        await page.keyboard.type(typeUrl, {delay: 10})
+
+        const editedContent = await getEditedPostContent()
+        
+        expect( editedContent.includes('{"addButton":true,')).toBe(true)
+        expect( editedContent.includes(`"buttonText":"${typeString}"`)).toBe(true)
+        expect( editedContent.includes(`"buttonLink":"${typeUrl}"}`)).toBe(true)
+        expect( await getEditedPostContent() ).toMatchSnapshot()
+    })
+
+    it( 'Richtext editor should update output correctly.', async () => {
+        await blockStartup(block)
+
+        const typeString = 'Title Test'
+
+        // focus the title richtext editor and type in it.
+        await page.focus('.block-editor-rich-text__editable.editor-title-nav__title')
+        await page.keyboard.type(typeString, {delay: 10})
+        
+        const editedContent = await getEditedPostContent()
+        
+        expect( editedContent.includes(`<h2 class="pu-title-nav__title">${typeString}</h2>`)).toBe(true)
+        expect( await getEditedPostContent() ).toMatchSnapshot()
     })
 } );
---
+++ src/title-nav/title-nav-link/block.spec.js
@@ -0,0 +1,85 @@
+// title-nav test
+jest.setTimeout(30000)
+import {
+    createNewPost,
+    enablePageDialogAccept,
+    getEditedPostContent,
+    insertBlock,
+    getAllBlocks,
+    selectBlockByClientId
+} from '@wordpress/e2e-test-utils';
+
+import {
+    clickElementByText,
+    blockStartup,
+    updateRangeInput,
+    clickCheckbox,
+    openSidebarPanelWithTitle,
+    openSidebar
+} from '../../test-helpers'
+
+const block = {blockTitle: 'Titled Navigation', blockName: 'purdue-blocks/title-nav'}
+
+describe( 'Titled Navigation Link Block', () => {
+    beforeAll( async () => {
+        await enablePageDialogAccept();
+    } );
+    beforeEach( async () => {
+        await createNewPost();
+    } );
+
+    it( 'Block should exist at minimum one.', async () => {
+        await blockStartup(block)
+
+        // tests that the block is properly inserted and matches the existing snapshot
+        expect(await page.$('[data-type="purdue-blocks/title-nav-link"]')).not.toBeNull()
+        expect( await getEditedPostContent() ).toMatchSnapshot()
+    } )
+
+    it( 'Current page checkbox should add correct style.', async () => {
+        await blockStartup(block)
+
+        // select title-nav-link
+        const inner = [...await getAllBlocks()][0].innerBlocks
+        await selectBlockByClientId(inner.clientId)
+
+        // force open block settings
+        await clickElementByText('button', 'Block')
+
+        // click title-nav-link to make sure block is focused and the settings appear in sidebar
+        const x = await page.$( '[data-type="purdue-blocks/title-nav-link"] .block-editor-rich-text__editable' )
+        await x.click()
+
+        // click the checkbox to show button controls
+        await clickCheckbox('Is this the current page?')
+
+        const editedContent = await getEditedPostContent()
+        
+        expect( editedContent.includes(`wp:purdue-blocks/title-nav-link {"isCurrent":true}`)).toBe(true)
+        expect( await getEditedPostContent() ).toMatchSnapshot()
+    })
+
+    it( 'Richtext editor should update output correctly.', async () => {
+        await blockStartup(block)
+
+        const typeString = 'Text'
+
+        // select title-nav-link
+        const inner = [...await getAllBlocks()][0].innerBlocks
+        await selectBlockByClientId(inner.clientId)
+
+        // force open block settings
+        await clickElementByText('button', 'Block')
+
+        // click title-nav-link to make sure block is focused and the settings appear in sidebar
+        const x = await page.$( '[data-type="purdue-blocks/title-nav-link"] .block-editor-rich-text__editable' )
+        await x.click()
+
+        await page.keyboard.type(typeString, {delay: 1})
+
+        const editedContent = await getEditedPostContent()
+        
+        expect( editedContent.includes(`<li class="">${typeString}</li>`)).toBe(true)
+        expect( await getEditedPostContent() ).toMatchSnapshot()
+    })
+})
