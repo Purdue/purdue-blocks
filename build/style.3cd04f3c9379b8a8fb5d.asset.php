f75fd8f36aeb625e097e0b06672df9ee9e3b72a8
Zong <yanhuazong@gmail.com>
2022-04-06 15:07:09 -0400

    Update faculty card style

--- build/style.3cd04f3c9379b8a8fb5d.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'ccd286326acce9a045011fc634874f6b');
---
+++ build/style.ce8481d1631d2f1e3d5c.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '4faa2cbdc87d11f779faa2172db42eb4');
