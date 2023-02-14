786f77860ee981938464b166d9f8aee61d421252
Zong <yanhuazong@gmail.com>
2023-02-14 12:19:08 -0500

    Fix CTA card read more button color

--- build/style.66359fc99bf245338aa2.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '5ec4d3947ffbadb5f46061972b946612');
---
+++ build/style.7f70af39021b3312d1ea.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '6e508604b6a5cc8fa05728f88044b14a');
