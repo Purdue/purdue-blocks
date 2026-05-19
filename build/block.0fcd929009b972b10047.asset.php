8b0e1dcb6796898823267b80ab5077c41c8201be
Nicole Gilles <negilles@purdue.edm>
2026-05-19 17:02:19 -0400

    Mini Hero - Remove aria-label

---
+++ build/block.0fcd929009b972b10047.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-element', 'wp-primitives', 'wp-server-side-render'), 'version' => '404f8eaa376bac6b664b');
--- build/block.b58baa6f05378e832762.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-element', 'wp-primitives', 'wp-server-side-render'), 'version' => 'ca85298aa3c927c40b7f');
